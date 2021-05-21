dea3b4723c61023eda508c68b3ce980549b21a5b
Greg <solary311@gmail.com>
2021-05-21 08:39:35 +0300

    Add Text in CircleView file

--- GitPractice/CircleView.swift
+++ GitPractice/CircleView.swift
@@ -13,6 +13,11 @@ struct CircleView: View {
         Circle()
             .fill(Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)))
             .padding()
+            
+            Text("ПРивет!")
+                .fontWeight(.semibold)
+            
+            Spacer()
         }
     }
 }
