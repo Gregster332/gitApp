06d162e963dfe3ae1fd9d90a811efea4c885b209
Greg <solary311@gmail.com>
2021-05-21 08:49:23 +0300

    Добавили рамку для круга и добавили кнопку

--- GitPractice/CircleView.swift
+++ GitPractice/CircleView.swift
@@ -8,11 +8,21 @@
 import SwiftUI
 
 struct CircleView: View {
+    
+   
+    
     var body: some View {
         VStack {
         Circle()
             .fill(Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)))
+            .frame(width: 100, height: 100, alignment: .center)
             .padding()
+            
+            Button(action: {}, label: {
+                Text("Кнопка")
+                    .fontWeight(.black)
+            })
+            
         }
     }
 }
